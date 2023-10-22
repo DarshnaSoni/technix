d69535edd9c7cf450e9cc51b16da903f0882b73f
DarshnaSoni <darshna@websoham.com>
2023-10-22 18:30:59 +0530

    Home 2 delete

--- src/components/homes/home/index.jsx
+++ src/components/homes/home/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import HeroSlider from "./hero-slider";
 import AboutArea from "./about-area";
 import FeatureArea from "./feature-area";
-import ServiceArea from "./service-area"; 
+import ServiceArea from "./service-area";
 import BrandArea from "./brand-area";
 import SupportArea from "./support-area";
 import TestimonialArea from "./testimonial-area";
@@ -13,24 +13,24 @@ import BlogArea from "./blog-area";
 import Footer from "@/src/layout/footers/footer";
 
 const HomeOne = () => {
-  return (
-    <>
-      <HeaderOne />
-      <main>
-        <HeroSlider />
-        <AboutArea />
-        <FeatureArea />
-        <ServiceArea />
-        <BrandArea />
-        <SupportArea />
-        <TestimonialArea />
-        <SliderArea />
-        <FaqArea />
-        <BlogArea />
-        <Footer />
-      </main>
-    </>
-  );
+	return (
+		<>
+			<HeaderOne />
+			<main>
+				<HeroSlider />
+				<AboutArea />
+				<FeatureArea />
+				<ServiceArea />
+				<BrandArea />
+				<SupportArea />
+				<TestimonialArea />
+				<SliderArea />
+				<FaqArea />
+				<BlogArea />
+				<Footer />
+			</main>
+		</>
+	);
 };
 
 export default HomeOne;
